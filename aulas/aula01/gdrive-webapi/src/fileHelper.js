32e075a9685500a5829cb0daed2e8db3bf17e72b
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-09-08 22:58:02 -0300

    map owner, size, calc

--- aulas/aula01/gdrive-webapi/src/fileHelper.js
+++ aulas/aula01/gdrive-webapi/src/fileHelper.js
@@ -1,5 +1,17 @@
-export default class FileHelper {
-    static async getFilesStatus(downloadsFolder){
+import fs from 'fs';
 
-    }
+export default class FileHelper {
+    static async getFilesStatus(downloadsFolder) {
+        const currentFiles = await fs.promises.readdir(downloadsFolder)
+        const statuses =
+
+            await Promise.all(
+                currentFiles
+                    .map(file => fs.promises.stat(`${downloadsFolder}/${file}`))
+            )
+                for ( const fileIndex in currentFiles){
+                    const { birthtime, size } = statuses[fileIndex]
+                    console.log({ birthtime, size })
+                }
+        }
 }
